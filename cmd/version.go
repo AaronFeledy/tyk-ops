c49c3bbf399319f308301946c8ca99172d18bc72
Aaron Feledy <aaron@arrowonesolutions.com>
2023-03-08 23:24:16 -0600

    TykOps is unique enough to have its own version

--- cmd/version.go
+++ cmd/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "1.2.4"
+const VERSION = "0.0.1"
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
@@ -15,8 +15,8 @@ func init() {
 var versionCmd = &cobra.Command{
 	Use:     "version",
 	Aliases: []string{"v"},
-	Short:   "Tyk-sync version",
-	Long:    `This command will show the current Tyk-Sync version.`,
+	Short:   "TykOps version",
+	Long:    `This command will show the current TykOps version.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("v" + VERSION)
 	},
